b0ebe2735d313e96afc82977d162103dc49d133a
vesta-12 <sofiya.serbina@narxoz.kz>
2026-03-17 23:09:47 +0500

    create processor with constructors

--- src/main/java/org/example/payment/PaymentProcessor.java
+++ src/main/java/org/example/payment/PaymentProcessor.java
@@ -1,4 +1,17 @@
-package org.example;
+package org.example.payment;
 
 public class PaymentProcessor {
+
+    private PaymentStrategy strategy;
+
+    public String processPayment(int amount) {
+        if (strategy == null) {
+            throw new IllegalStateException("error - pick type of payment");
+        }
+        return strategy.pay(amount);
+    }
+
+    public void setStrategy(PaymentStrategy strategy) {
+        this.strategy = strategy;
+    }
 }
